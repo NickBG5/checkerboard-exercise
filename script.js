3081df8a2fed254b8d721d7f655dc49c5449f1c6
NickBG5 <nbg123097@gmail.com>
2021-09-02 18:21:09 -0500

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 // Your JS goes here
-for(let i = 0; i <= 63; i++) {
-    let div = document.createElement('div');
-    document.body.style.width = '100vh';
-    document.body.style.height = '100vh';
-    div.style.height = '11.1%';
-    div.style.width = '11.1%';
-    div.style.backgroundColor = 'blue';
-    div.style.float = 'left';
-    div.style.border = '1px solid black';
-    document.body.appendChild(div);
+document.body.style.width = "100vh";
+document.body.style.height = "100vh";
+    
+for(let i = 0; i < 8; i++){
+    for(let j = 0; j < 8; j++){
+        let check = document.createElement("div");
+
+    check.style.width = "12.5%"
+    check.style.height = "12.5%"
+    check.style.float = "left";
+
+    if(i % 2 === 0){
+        let color = j % 2 === 0 ? "red" : "black";
+        check.style.backgroundColor = color;
+    }
+    else {
+        let color = j % 2 == 0 ? "black" : "red";
+        check.style.backgroundColor = color;
+    }
+        document.body.appendChild(check);
+    }
 }
