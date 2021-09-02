b7b9f2becb612f72d1fc4f599bb3889c87914a3e
NickBG5 <nbg123097@gmail.com>
2021-09-02 16:30:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Your JS goes here
+for(let i = 0; i <= 63; i++) {
+    let div = document.createElement('div');
+    document.body.style.width = '100vh';
+    document.body.style.height = '100vh';
+    div.style.height = '11.1%';
+    div.style.width = '11.1%';
+    div.style.backgroundColor = 'blue';
+    div.style.float = 'left';
+    div.style.border = '1px solid black';
+    document.body.appendChild(div);
+}
